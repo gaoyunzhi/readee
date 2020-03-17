ea1c63a888a931cbd0188b6510cbf9f766e37391
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-03-17 13:57:25 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="readee",
-    version="0.0.24",
+    version="0.0.25",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Library for export webpage to reader mode html.",
