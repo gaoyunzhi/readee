fd50a0e578922957bed54283bbfa9647663a6ddf
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-04-25 01:39:12 -0700

    commit

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import os
 import sys
 
 urls = [
-	'https://www.zhihu.com/question/24762672/answer/33939678'
+	'https://mp.weixin.qq.com/s?__biz=MzI2MTExMTE3Ng==&amp;mid=2247483688&amp;idx=1&amp;sn=4f1146d79e1d37aa23c61467c876c2a2&amp;chksm=ea5e2dcfdd29a4d96243437cd6f4109925c4e70592e34e12039500c5917412d25174c9d3e885&amp;mpshare=1&amp;scene=1&amp;srcid=&amp;sharer_sharetime=1587789413659&amp;sharer_shareid=7c19efe830ee0c8a2796cbc54e04c282#rd'
 ]
 
 def getFileName(url):
