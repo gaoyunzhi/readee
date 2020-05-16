383e67852d7717e2aa8b701a56646dd322790f34
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-05-16 15:29:36 -0700

    commit

--- readee/domain.py
+++ readee/domain.py
@@ -2,26 +2,18 @@
 # -*- coding: utf-8 -*-
 
 from .common import _findRawContent
-
-def _parseDomain(url):
-	if not url:
-		return 
-	if not url.startswith('http'):
-		return
-	r = '/'.join(url.split('/')[:3])
-	if r.count('/') == 2 and 'http' in r:
-		return r
+from telegram_util import parseDomain
 
 def _findDomain(soup, url):
 	for meta in soup.find_all('meta'):
 		for att in meta.attrs:
 			if 'url' in att.lower():
-				r = _parseDomain(meta[att])
+				r = parseDomain(meta[att])
 				if r:
 					return r
 	for meta in soup.find_all('meta'):
 		if 'url' in str(meta).lower():
-			r = _parseDomain(_findRawContent(meta))
+			r = parseDomain(_findRawContent(meta))
 			if r:
 				return r
-	return _parseDomain(url)
+	return parseDomain(url)
